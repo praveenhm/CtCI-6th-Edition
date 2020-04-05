a562c90f5fddd41d494110720927854d11c9c695
praveenhm <praveenhm2@yahoo.com>
2020-04-04 23:18:31 -0700

    my solution

---
+++ Java/Ch 01. Arrays and Strings/Q1_05_One_Away/MySol.java
@@ -0,0 +1,23 @@
+package Q1_05_One_Away;
+
+public class MySol {
+    public static void main(String[] args) {
+
+        String[] remove = {"pale","ple"};
+        oneWay(remove[0],remove[1]);
+    }
+
+    static boolean oneWay(String s1, String s2){
+
+        if(s1.length() < s2.length() )
+            return true;
+        else if(s1.length() >)
+
+        //remove check if it is one charcter removed
+        if(s1.length() < s2.length()){
+        for (int i = 0; i < s1.length(); i++) {
+
+        }
+        if
+    }
+}
